604491dab457a9f765d68885ffa2f65d421dc641
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-01 11:03:12 +0530

    codelab 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
+      debugShowCheckedModeBanner: false,
       home: RandomWords(),
     );
   }
